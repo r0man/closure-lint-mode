7a43c48b5a7b70281a2f269947d4f479bfa831ae
Roman Scherer <roman@burningswell.com>
2010-11-18 21:49:54 +0100

    Added key bindings and auto loading.

--- closure-lint-mode.el
+++ closure-lint-mode.el
@@ -21,45 +21,97 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+;;; Commentary:
+
+;; An Emacs minor mode to check and fix the syntax of Javascript
+;; buffers with the Closure Linter.
+;;
+;; The mode uses flymake to check the syntax of Javascript buffers
+;; with the "gjslint" program. Errors will be highlighted via flymake
+;; and some of them can be resolved by calling the "fixjsstyle"
+;; program.
+;;
+;; Key bindings:
+;;
+;; C-c C-e  -  Show the error message of a line with errors.
+;; C-c C-f  -  Fix the current buffer by calling fixjsstyle.
+
 (require 'flymake)
 
-(defgroup closure-lint-mode nil
-  "A minor mode for the Closure Linter"
-  :prefix "closure-lint-mode-"
-  :group 'applications)
-
-(defcustom closure-lint-mode-gjs-lint "gjslint"
+(defcustom closure-lint-gjs-lint "gjslint"
   "The name of the Closure Linter program."
   :type 'string
-  :group 'closure-lint-mode)
+  :group 'closure-lint)
 
-(defcustom closure-lint-mode-fix-js-style "fixjsstyle"
+(defcustom closure-lint-fix-js-style "fixjsstyle"
   "The name of the Closure Fix JS Style program."
   :type 'string
-  :group 'closure-lint-mode)
+  :group 'closure-lint)
 
-(defun closure-lint-mode-fix-buffer (filename)
-  (let* ((command (concat closure-lint-mode-fix-js-style " " filename))
+(defvar closure-lint-mode-map
+  (let ((map (make-sparse-keymap)))    
+    (define-key map "\C-c\C-e" 'flymake-display-err-menu-for-current-line)
+    (define-key map "\C-c\C-f" 'closure-lint-fix-buffer)
+    map)
+  "The keymap used in `closure-lint-mode buffers.")
+
+(define-minor-mode closure-lint-mode
+  "Closure Lint mode.
+     With no argument, this command toggles the mode.
+     Non-null prefix argument turns on the mode.
+     Null prefix argument turns off the mode."
+  :init-value nil
+  :lighter " GJSLint"
+  :keymap '(("\C-c\C-f" 'closure-lint-fix-buffer))
+  :group 'closure-lint)
+
+(defun closure-lint-find-file-hook ()
+  "Hook function to enable closure-lint-mode and flymake-mode if
+the current buffer's file is a Javascript file."
+  (when (string-match "\\.js$" buffer-file-name)    
+    (closure-lint-mode 't)
+    (flymake-mode 't)))
+
+(defun closure-lint-fix-file (filename)
+  "Run the \"fixjsstyle\" program on `filename."
+  (let* ((command (concat closure-lint-fix-js-style " " filename))
          (output (shell-command-to-string command)))
-    output))
+    (message output)))
 
-(defun closure-lint-mode-flymake-init ()
+(defun closure-lint-fix-buffer ()
+  "Run the \"fixjsstyle\" program on the content of the current
+  buffer and replace the current buffer's content with the fixed
+  content."
+  (interactive)
+  (let* ((current-point (point))
+         (temp-file (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace)))
+    (progn
+      (closure-lint-fix-file temp-file)
+      (erase-buffer)
+      (insert-file-contents temp-file)
+      (goto-char current-point)
+      (delete-file temp-file))))
+
+(defun closure-lint-flymake-init ()
+  "Flymake init function using the closure linter."
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
-         (local-file (file-relative-name temp-file
-                                         (file-name-directory buffer-file-name))))
-    (list closure-lint-mode-gjs-lint (list (expand-file-name local-file)))))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list closure-lint-gjs-lint (list (expand-file-name local-file)))))
+
+(add-hook 'find-file-hooks 'closure-lint-find-file-hook)
 
 (setq flymake-allowed-file-name-masks
       (cons '(".+\\.js$"
-              closure-lint-mode-flymake-init
+              closure-lint-flymake-init
               flymake-simple-cleanup
               flymake-get-real-file-name)
             flymake-allowed-file-name-masks))
 
 (setq flymake-err-line-patterns
-      (cons '("^Line \\([[:digit:]]+\\), E:\\([[:digit:]]+\\):
-\\(.+\\)$" nil 1 nil 3)
+      (cons '("^Line \\([[:digit:]]+\\), E:\\([[:digit:]]+\\):\\(.+\\)$" nil 1 nil 3)
             flymake-err-line-patterns))
 
 (provide 'closure-lint-mode)
